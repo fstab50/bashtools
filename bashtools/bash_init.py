847edaaaf9e7b47e68bfa3a1bc43967c84a92536
Blake Huber <blake.huber@atos.net>
2018-05-18 15:40:47 -0500

    bash_init.py

--- bashtools/bash_init.py
+++ bashtools/bash_init.py
@@ -26,15 +26,15 @@ def parameters(args):
         parameter_str += arg + ' '
     return parameter_str
 
-    
-cmd = 'sudo sh rkhunter-install.sh ' + parameters(sys.argv[1:])
-
 
 
 # Method 1:  Call --------------------------------------------------------------
 
     # can't process stdout stream
     # CAN perform user interface operations for interaction
+
+
+cmd = 'sudo sh rkhunter-install.sh ' + parameters(sys.argv[1:])
     
 subprocess.call(
         [cmd], shell=True,
@@ -46,10 +46,9 @@ sys.exit(0)
 
 # Method 2: subprocess.Popen  --------------------------------------------------
 
-
-
     # - can process output if required
     # - Need to know how to SIGINT after execution -- shell script retains control
+
 
 
 process = subprocess.Popen(
