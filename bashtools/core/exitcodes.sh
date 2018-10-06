8e68db9b8fd4fbd0bac0ab94afcfb1fdf84fa38b
Blake Huber <blakeca00@gmail.com>
2018-10-06 06:25:15 -0500

    bashtools/core/exitcodes.sh: updated with new OSERROR code

--- bashtools/core/exitcodes.sh
+++ bashtools/core/exitcodes.sh
@@ -6,14 +6,15 @@
 #
 
 # error codes
-E_OK=0                             # exit code if normal exit conditions
-E_DEPENDENCY=1                     # exit code if missing required ec2cdependency
-E_NOLOG=2                          # exit code if failure to create log dir, log file
-E_BADSHELL=3                       # exit code if incorrect shell detected
-E_AUTH=4                           # exit code if authentication failure
-E_CONFIG=5                         # exit code if configuration file missing or corrupted
-E_USER_CANCEL=7                    # exit code if user cancel
-E_BADARG=8                         # exit code if bad input parameter
-E_NETWORK_ACCESS=9                 # exit code if no network access from current location
-E_EXPIRED_CREDS=10                 # exit code if temporary credentials no longer valid
-E_MISC=11                          # exit code if miscellaneous (unspecified) error
+E_OK='0'                  # exit code if normal exit conditions
+E_DEPENDENCY='1'          # exit code if missing required ec2cdependency
+E_NOLOG='2'               # exit code if failure to create log dir, log file
+E_BADSHELL='3'            # exit code if incorrect shell detected
+E_AUTH='4'                # exit code if authentication failure
+E_CONFIG='5'              # exit code if configuration file missing or corrupted
+E_OSERROR='6'             # exit code if fail to identify os or os-specific attribute
+E_USER_CANCEL='7'         # exit code if user cancel
+E_BADARG='8'              # exit code if bad input parameter
+E_NETWORK_ACCESS='9'      # exit code if no network access from current location
+E_EXPIRED_CREDS='10'      # exit code if temporary credentials no longer valid
+E_MISC='11'               # exit code if miscellaneous (unspecified) error
