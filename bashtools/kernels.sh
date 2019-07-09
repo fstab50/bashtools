53c460887a507ec839da578c8a33f56085492753
Blake Huber <blakeca00@gmail.com>
2019-07-09 17:37:01 -0500

    bashtools/kernels.sh: added

---
+++ bashtools/kernels.sh
@@ -0,0 +1,106 @@
+#!/usr/bin/env bash
+
+#
+#   Discover and Remove old Kernels (free up /boot)
+#       - requires sudo or root
+#
+#
+
+pkg=$(basename $0)                                  # pkg (script) full name
+pkg_root=$(echo $pkg | awk -F '.' '{print $1}')     # pkg without file extention
+pkg_path=$(cd $(dirname $0); pwd -P)                # location of pkg
+OS_INFO=$(sh $pkg_path/os_distro.sh 2>/dev/null)
+os_family=$(echo $OS_INFO | awk '{print $1}')
+os_release=$(echo $OS_INFO | awk '{print $2}')
+
+# source
+source $pkg_path/core/std_functions.sh
+source $pkg_path/core/colors.sh
+
+
+function root_permissions_bool(){
+    ##
+    ## validates required root privileges, return bool only ##
+    ##
+
+    if [ $EUID -eq 0 ] || [ $SUDO_USER ]; then
+
+        return 0
+
+    fi
+    return 1
+    #
+    # <-- end function root_permissions -->
+}
+
+
+function list_deprecated_kernels(){
+    ##
+    ##  List all Kernels installed, but no longer in use
+    ##
+    declare -a kernels
+    mapfile -t kernels < <($(dpkg --list 'linux-image*'| awk '{ if ($1=="ii") print $2}'|grep -v $(uname -r)))
+    echo "${kernels[@]}"
+    #
+    # <-- end function -->
+}
+
+
+function uninstall_kernel(){
+    declare -a kernels=("${!1}")
+    local option="$2"
+
+    kernel="${kernels[$option]}"
+
+    std_message "You are about to uninstall $kernel" "WARN"
+    read -p "Confirm? [yes]: " choice
+
+    if [ ! $choice ]; then
+        apt-get purge $kernel
+        return 0
+    else
+        std_message "Kernel $kernel left installed"
+        return 1
+    fi
+}
+
+
+function finishing_actions(){
+    ##
+    ##  required actions if uninstall a kernel
+    ##
+    apt-get autoremove
+    update-grub
+    std_message "Completed autoremove and grub update" "INFO"
+}
+
+
+# -- main -----------------------------------------------------------------------------------------
+
+
+if root_permissions_bool; then
+
+    declare -a arr_kernels
+    mapfile -t arr_kernels < <(list_deprecated_kernels)
+    std_message "Installed Kernels No longer in use:" "INFO"
+    i=0
+    for k in "${arr_kernels[@]}"; do
+        printf -- '(%s):\t%s\n' "$i" "$k"
+        i=$(( $i + 1 ))
+    done
+
+    #select_kernel
+    exit 0
+    if uninstall_kernel; then
+        finishing_actions
+    fi
+
+else
+    std_warn "This command requires root or sudo access to root privileges.\n
+        \n\t\tRe-run as root or execute with sudo:
+            \n\t\t\t$ sudo  $pkg  <parameters>"
+        printf -- "\n\n"
+    exit 1
+fi
+
+exit 0
